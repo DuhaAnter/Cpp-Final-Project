574598dfd270e658ab425fe2bab0ea90c0eb1ff6
Duha <duhaanter@gmail.com>
2026-01-15 04:12:43 +0200

    managed write and read Income

---
+++ manageIncome/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+//C++ has a tool called string stream that treats a string like a mini-file. You can "read" from it just like cin.
+#include<sstream>
+using namespace std;
+//A way to organize data (struct = the blueprint)
+struct Income
+{
+    int id;
+    double amount;
+    string date;
+    string source;
+
+};
+int main()
+{
+    Income inc1;
+    inc1.amount=14000;
+    inc1.date ="15jan2026";
+    inc1.source="salary";
+    inc1.id=2;
+
+    //1.
+    ofstream file ;
+    //2.
+    file.open("income.txt",ofstream::app);
+    //3.did it worked?
+    if(file.fail())
+    {
+        cout << "Error opening the file ";
+        return 1;
+    }
+
+    /*
+    //4.if no . write in it
+    file << inc1.id << "|" <<inc1.source<<"|"<<inc1.amount<<"|"<<inc1.date<<endl;
+    */
+    //5.
+    file.close();
+////////////////////////////////////////////////////read//////////////////////////////
+
+    ifstream readfromFile;
+
+    readfromFile.open("income.txt");
+    if(readfromFile.fail())
+    {
+        cout<<"Error opening the file";
+        return 1; // terminate program
+    }
+    string fileLine;
+
+    getline(readfromFile,fileLine);
+    if(readfromFile.fail())
+    {
+        cout<<"Error Reading from the file";
+        return 1;
+    }
+
+    /*
+    // 1. The string you read from file
+    string fileLine = "3|salary|15000|15jan2026";
+    */
+// 2. Create string stream
+    stringstream ss(fileLine);
+
+// 3. Variables to hold each piece
+    string idStr, source, amountStr, date;
+
+// 4. Extract pieces using | as delimiter
+    getline(ss, idStr, '|');    // Gets "1"
+    getline(ss, source, '|');   // Gets "salary"
+    getline(ss, amountStr, '|'); // Gets "10000"
+    getline(ss, date, '|');     // Gets "15jan2026"
+
+// 5. Convert strings to appropriate types
+    int id = stoi(idStr);           // "1" → 1
+    double amount = stod(amountStr); // "10000" → 10000.0
+
+// 6. Now store in your struct
+    Income income;
+    income.id = id;
+    income.source = source;
+    income.amount = amount;
+    income.date = date;
+
+    cout<<income.amount;
+    return 0;
+}
--- write&readFromFiles/main.cpp
+++ write&readFromFiles/main.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 int main()
 {
+    /*
+
+
     // create the ofstream object
+
     ofstream outFile;
+
     // open file.txt using the append flag (ofstream::app)so content will be appended to the
     // end of any existing content in file.txt
     // if i did not use it . every time will overwrite everything in the file
@@ -37,5 +42,64 @@ int main()
 
     // close the file, as a best practice when we are done with it (freeing memory)
     outFile.close();
+
+
+    */
+////////////////////////////////////READ FROM FILE/////////////////////////////////////////////////
+// NOTE : u have to  know the order of the variables in the file so u can read them using the correct data types otherwise it won't work.
+
+    // create an ifstream object
+    ifstream readFile;
+
+
+    readFile.open("file.txt");
+
+    // if file.txt fails to open, fail() will return true, and we'll exit
+    // gracefully by telling the user what went wrong and exiting with status 1
+    // which is a signal to the shell that something wen't wrong in the execution
+    // of our program
+    if (readFile.fail())
+    {
+        cout << "Error opening file." << endl;
+        return 1; //terminate program
+    }
+
+
+
+    string line;
+    // If the file contents are:
+    // This is a string.
+    //
+    // Then if we use the stream extraction operator (>>), only the keyword "This"
+    // will be stored into line... because the operator will stop at the first
+    // whitespace character.  We can use getline() to read in the entire line
+    // including spaces, because it will stop at the first newline in the file.
+    //
+    getline(readFile, line);
+    cout << line << endl;
+
+
+    double x=0;
+
+    readFile >> x;
+
+    cout<<x<<endl;
+
+
+     // variable to store number in the file
+    int number = 0;
+
+    // read in the number
+    readFile >> number;
+    //check if it's read
+    if (readFile.fail())
+    {
+        cout << "Error file format incorrect." << endl;
+        return 1;
+    }
+
+    cout<<number;
+
+
     return 0;
 }
